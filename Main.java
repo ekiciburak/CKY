9bace6c1272bb99e6bc3abd0732e8d10993e2f26
ekiciburak <ekcburak@hotmail.com>
2022-12-02 23:08:23 +0300

    bulk upload...

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		boolean b;
+	    String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+	    //String s = "baaba"; // test it with the grammar discussed in the class; see cky.java file
+
+		cky c = new cky();
+		
+		System.out.printf("input string s: %s", s);
+		System.out.println();
+		
+		b = c.ckyAlg(s); //calling the CKY parser.
+	
+		System.out.printf("s in L(G): ");
+		if(b)
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+
+}
---
+++ cky.java
@@ -0,0 +1,172 @@
+public class cky {
+	
+	//grammar G in CNG s.t. L(G) = { a^ib^i | i > 0 }.
+    String [] S = {"AC", "AB"};
+    String [] A = {"a"};
+    String [] B = {"b"};
+    String [] C = {"SB"};
+	
+	/*
+    //example discussed in the class
+    String [] S = {"AB", "BC"};
+    String [] A = {"BA", "a"};
+    String [] B = {"CC", "b"};
+    String [] C = {"AB", "a"};
+    */
+	
+    int size = 50; // have the size larger if the string length is "very" long.
+
+    //auxiliary arrays
+	String [] ACC  = new String[20]; 
+    String [] ACC1 = new String[20]; 
+    String [] ACC2 = new String[20]; 
+    String [] BCC  = new String[20]; 
+
+    public String[] search (String s)
+    {
+        String [] ACC = new String[size]; 
+    	int k = 0;
+    	for (int i = 0; i < S.length; i++) 
+    	{
+    		if(s.equals(S[i]))
+    		{
+    			ACC[k] = "S";
+    			k++;
+    		}
+    	}
+    	for (int i = 0; i < A.length; i++) 
+    	{
+    		if(s.equals(A[i]))
+    		{
+    			ACC[k] = "A";
+    			k++;
+    		}
+    	}
+    	for (int i = 0; i < B.length; i++) 
+    	{
+    		if(s.equals(B[i]))
+    		{
+    			ACC[k] = "B";
+    			k++;
+    		}
+    	}
+    	for (int i = 0; i < C.length; i++) 
+    	{
+    		if(s.equals(C[i]))
+    		{
+    			ACC[k] = "C";
+    			k++;
+    		}
+    	}
+		
+		return ACC;
+    }
+    
+    public int lenc (String [] s)
+    {
+    	int i;
+    	for (i=0; s[i] != null; i++);
+    	return i;
+    }
+    
+    public String[] concat (String [] s1, String [] s2)
+    {
+    	int l1 = lenc(s1);
+    	int l2 = lenc(s2);
+        String [] ACC = new String[size];
+        int k = 0;  
+        for (int i = 0; i < l1; i++) 
+        {
+    		for (int j = 0; j < l2; j++) 
+    		{
+    			ACC[k] = s1[i].concat(s2[j]);
+    			k++;	
+			}  			
+        }
+    	return ACC;
+    }
+    
+    public String[] append (String [] s1, String [] s2)
+    {
+    	int l1 = lenc(s1);
+    	int l2 = lenc(s2);
+        String [] ACC = new String[l1+l2+1];
+        int j = 0;
+        
+        if (l1 == 0)
+        	ACC = s2;
+        else if (l2 == 0)
+        	ACC = s1;
+        else
+        {
+            for (int i = 0; i < l1+l2; i++) 
+            {
+            	if (i < l1)
+            		ACC[i] = s1[i];
+            	else
+            	{
+            		ACC[i] = s2[j];
+            		j++;
+            	}
+    		}      	
+        }
+        return ACC;
+    }
+    
+    public String [] clear (String [] s)
+    {
+    	int l1 = lenc(s);
+    	for (int i = 0; i < l1; i++)
+			s[i] = null;
+    	return s;
+    }
+    
+    //CKY parser
+    public Boolean ckyAlg (String s)
+    {
+        int index = s.length()+1;
+        int l = s.length();
+        Boolean b = false;
+        
+        //2-dim array (of strings -> [3-dim indeed]) storing non-terminals generating substrings of s.
+        String [][][] T = new String[index][index][50];
+        
+		for (int i = 0; i < s.length(); i++) {
+			T[i][i+1] = search(Character.toString(s.charAt(i)));
+		}
+		
+		for (int u = 2; u < index; u++) 
+		{
+			for (int m = 0; m < index-u; m++) 
+			{
+				for (int i = 1; i < u; i++) 
+				{
+					BCC = concat(T[m][m+i], T[m+i][m+u]);
+					ACC = append(ACC, BCC);
+				}
+				for (int i = 0; ACC[i] != null; i++) 
+				{
+					ACC1 = search(ACC[i]);
+					if(ACC1[0] != null)
+					{
+						ACC2 = append(ACC2, ACC1);
+					}
+				}
+				T[m][m+u] = ACC2.clone();		
+				clear(ACC2);
+				clear(BCC);
+				clear(ACC);
+			}
+		}
+		
+		System.out.printf("list of non-terminals generating s (may contain dups): ");
+		for (int m = 0; T[0][l][m] != null; m++)
+		{
+			System.out.printf("%s ", T[0][l][m]);
+			if (T[0][l][m].equals("S"))
+				b = true;
+		} 
+		System.out.println();
+		return b;
+    }
+}
